2177ec9ea15e783d57655c21fe659238477c95e7
Clement <clementmaganza@gmail.com>
2025-12-17 11:57:06 +0100

    implementation of the function to create a collection :construction:

---
+++ src/middleware/authenticateToken.js
@@ -0,0 +1,29 @@
+import { response } from 'express'
+import { request } from 'express'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+/**
+ * 
+ * @param {request} req 
+ * @param {response} res 
+ * @param {*} next 
+ */
+export const authenticateToken = (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization
+        const token = authHeader && authHeader.split(' ')[1]
+
+        if(!token){
+            return res.status(401).json({ error: 'Access token required' })
+        }
+
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        const userId = decodedToken.userId
+
+        req.user = {userId}
+        next()
+    } catch(error){
+        res.status(401).json({error: 'Invalid token'})
+    }
+}
--- src/routers/collectionRouter.js
+++ src/routers/collectionRouter.js
@@ -1,10 +1,13 @@
 import Router from "express";
 import { createCollection } from "../controllers/collectionControllers.js";
 import { createCollectionSchema } from "../models/collection.js";
-import { validateBody } from "../middleware/vadidation.js";
+import { validateBody } from "../middleware/validation.js";
+import { authenticateToken } from "../middleware/authenticateToken.js";
 
 
 const router = Router()
+
+router.use(authenticateToken)
 
 router.post("/",validateBody(createCollectionSchema),createCollection);
 
